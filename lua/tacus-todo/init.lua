d4038648a800bb1c56a701076ea7c4d3e927bac7
154Sportacus <2239560@gmail.com>
2026-02-01 02:14:42 +0000

    first release, custom ascci art

---
+++ lua/tacus-todo/init.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+M.format_todo = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    
+local header = {
+        "  _           _        ",
+        " | |_ ___  __| | ___   ",
+        " | __/ _ \\/ __ |/ _ \\  ",
+        " | || (_) | (_|| (_) | ",
+        "  \\___\\___/\\__,_|\\___/ ",
+        "-----------------------",
+        "",
+    }
+    local tasks = {}
+    for _, line in ipairs(lines) do
+        -- Limpa números e espaços antigos
+        local task = line:gsub("^%d+%.%s*", ""):gsub("^%s+", "")
+        -- Só adiciona se não for lixo ou parte do cabeçalho
+        if task ~= "" and not task:find("|") and not task:find("%-%-%-") and not task:find("_") then
+            table.insert(tasks, task)
+        end
+    end
+
+    local new_content = {}
+    for _, h in ipairs(header) do table.insert(new_content, h) end
+    for i, t in ipairs(tasks) do
+        table.insert(new_content, i .. ". " .. t)
+    end
+
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, new_content)
+end
+
+M.setup = function()
+    vim.api.nvim_create_user_command('TacusTodoList', M.format_todo, {})
+    
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*/Desktop/todo",
+        callback = M.format_todo,
+    })
+end
+
+return M
